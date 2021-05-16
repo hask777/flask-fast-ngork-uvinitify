3122bbecca83c54e584159b7a499c048e170006e
hask777 <hask777@kali.hask777>
2021-05-16 08:00:44 -0400

    init

---
+++ scrape.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def scrape_brands():
+    base_url = 'https://av.by/'
+
+    headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+        }
+
+    r = requests.get(base_url)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+    brandlist = soup.find('ul', class_='brandslist')
+
+    lis = soup.find_all('li', class_="brandsitem")
+    print(lis)
+
+    brands_dict = {}
+    list_brands = []
+
+    for item in lis:
+        name = item.find('span').text
+        # print(name)
+
+        cars_count = item.find('small').text
+        # print(count)
+        if int(cars_count) > 25:
+            brands_dict = {
+                'name': name.lower(),
+                'cars_count': int(cars_count)
+            }
+
+        # print(brands_dict)
+
+            list_brands.append(brands_dict)
+
+    print(list_brands)
+
+
+    return list_brands
+
+
+
+def dump_json():
+    
+    brands = "brands.json"
+    with open(brands, 'w', encoding='utf-8') as json_file:
+        json.dump(scrape_brands(), json_file, ensure_ascii = False, indent =4)
+    print("dump")
+
+dump_json()
+
